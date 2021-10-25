9e4fcf37ec6641873a229985d80b369d599359a9
hcanon42 <hcanon@student.42.fr>
2021-10-25 16:00:44 +0200

    changed a bit

--- srcs/packer.c
+++ srcs/packer.c
@@ -5,7 +5,7 @@ char		jmp[] = "xe9xffxffxffxff";
 char		pusha[] = "x60";
 char		popa[] = "x61";
 
-# define CODE_OFFSET (elf_pt_load->p_offset + elf_pt_load->p_memsz)
+# define CODE_OFFSET (text_seg->p_offset + text_seg->p_memsz)
 # define CODE_ADRESS (old_entry + CODE_OFFSET)
 
 int		initialize_packer(t_packer *pack, char *file_to_open)
@@ -33,7 +33,7 @@ int		initialize_packer(t_packer *pack, char *file_to_open)
 
 void	openFile(char *to_read, t_packer *pack)
 {
-	pack->_fd = open(to_read, O_RDONLY);
+	pack->_fd = open(to_read, O_APPEND | O_RDWR);
 	if (pack->_fd == -1)
 	{
 		pack->_type = 0;
@@ -43,17 +43,17 @@ void	openFile(char *to_read, t_packer *pack)
 	lseek(pack->_fd, 0, SEEK_SET);
 }
 
-int		is_space_available(Elf64_Phdr *elf_pt_load, size_t code_size)
-{
-	Elf64_Phdr		*elf_pt_load_next;
+// int		is_space_available(Elf64_Phdr *elf_pt_load, size_t code_size)
+// {
+// 	Elf64_Phdr		*elf_pt_load_next;
 
-	elf_pt_load_next = elf_pt_load + 1;
-	if (elf_pt_load->p_type == PT_LOAD && elf_pt_load_next->p_type == PT_LOAD)
-		if (elf_pt_load->p_memsz != elf_pt_load->p_filesz ||
-			(CODE_OFFSET + code_size) > (elf_pt_load_next->p_offset + elf_pt_load->p_offset))
-		return (1);
-	return (0);
-}
+// 	elf_pt_load_next = elf_pt_load + 1;
+// 	if (elf_pt_load->p_type == PT_LOAD && elf_pt_load_next->p_type == PT_LOAD)
+// 		if (elf_pt_load->p_memsz != elf_pt_load->p_filesz ||
+// 			(CODE_OFFSET + code_size) > (elf_pt_load_next->p_offset + elf_pt_load->p_offset))
+// 		return (1);
+// 	return (0);
+// }
 
 void	insert_code(unsigned char *ptr)
 {
@@ -69,50 +69,73 @@ void	insert_code(unsigned char *ptr)
 int		inject_code(unsigned char *f_mmaped, t_packer *pack)
 {
 	int				jmp_adr;
+	int				text_end = 0;
+	unsigned long	gap = pack->_size;
 	unsigned int	old_entry;
 	size_t			code_size;
 	Elf64_Ehdr		*elf_begin;
 	Elf64_Phdr		*elf_pt_load;
-	Elf64_Phdr		*elf_pt_load_next;
+	Elf64_Phdr		*text_seg;
 
+	printf("gap = %lu\n", gap);
 	code_size = ft_strlen(shellcode) + ft_strlen(jmp) + ft_strlen(pusha) + ft_strlen(popa);
-	elf_begin = (void*)f_mmaped;
+	elf_begin = (Elf64_Ehdr *)f_mmaped;
 	old_entry = elf_begin->e_entry;
 	printf("entry point = %.8x\n", old_entry);
-	if ((unsigned int)pack->_size < (elf_begin->e_phoff + elf_begin->e_phnum * elf_begin->e_phentsize))
-	{
-		ft_putstr("[-] ELF malformed.\n");
-		return (-1);
-	}
-	elf_pt_load = (void*)f_mmaped + elf_begin->e_phoff;	/* going to beginning of segment */
+	// if ((unsigned int)pack->_size < (elf_begin->e_phoff + elf_begin->e_phnum * elf_begin->e_phentsize))
+	// {
+	// 	ft_putstr("[-] ELF malformed.\n");
+	// 	return (-1);
+	// }
+	elf_pt_load = (Elf64_Phdr *) ((unsigned char *)elf_begin + (unsigned int)elf_begin->e_phoff);	/* going to beginning of segment */
 
 	ft_putstr("[+] Finding a free space under a PT_LOAD segment...\n");
-	for (int i = 0; i < elf_begin->e_phnum - 2; i++) 	/* searching for PT_LOAD */
+	for (int i = 0; i < elf_begin->e_phnum; i++) 	/* searching for PT_LOAD */
 	{
-		if (is_space_available(elf_pt_load, code_size) == 1)
-			break;
-		elf_pt_load++;
-	}
-	if (!(elf_pt_load + 1) || is_space_available(elf_pt_load, code_size) != 1)
-	{
-		ft_putstr("[-] Didn't found enough space.\n");
-		return (-1);
-	}
-	elf_pt_load_next = elf_pt_load + 1;
-	ft_printf("[+] Free space found : %d bytes.\n", (elf_pt_load_next->p_offset) - (CODE_OFFSET));
+		if (elf_pt_load->p_type == PT_LOAD && elf_pt_load->p_flags & 0x011)
+		{
+			printf ("+ Found .text segment (#%d)\n", i);
+			text_seg = elf_pt_load;
+			text_end = elf_pt_load->p_offset + elf_pt_load->p_filesz;
+		}
+		else
+		{
+			if (elf_pt_load->p_type == PT_LOAD && 
+				(elf_pt_load->p_offset - text_end) < gap) 
+			{
+				printf ("   * Found LOAD segment (#%d) close to .text (offset: 0x%x)\n",
+					i, (unsigned int)elf_pt_load->p_offset);
+				gap = elf_pt_load->p_offset - text_end;
+			}
+		}
+		elf_pt_load++; // = (Elf64_Phdr *) ((unsigned char*) elf_pt_load 
+					//	+ (unsigned int) elf_begin->e_phentsize);
+		printf("\nelf_pt_load->p_type = %d\n", elf_pt_load->p_type);
+		printf("elf_pt_load->p_offset = %ld\n", elf_pt_load->p_offset);
+		printf("elf_pt_load->p_memsz = %ld\n", elf_pt_load->p_memsz);
+		printf("elf_pt_load->p_vaddr = 0x%lx\n", elf_pt_load->p_vaddr);
+		printf("elf_pt_load->p_paddr = 0x%lx\n", elf_pt_load->p_paddr);
+
+    }
+	// if (!(elf_pt_load + 1) || is_space_available(elf_pt_load, code_size) != 1)
+	// {
+	// 	ft_putstr("[-] Didn't found enough space.\n");
+	// 	return (-1);
+	// }
+	ft_printf("[+] Free space found at 0x%x: %d bytes.\n", text_seg->p_offset, gap);
 
 	ft_printf("[+] Overwriting entry point (0x%.8x) program with shellcode adress (0x%.8x)...\n", old_entry, CODE_ADRESS);
-	elf_begin->e_entry = (old_entry + elf_pt_load->p_offset + elf_pt_load->p_memsz);	/* changing old entry of the code by the address of ours */
+	elf_begin->e_entry = (old_entry + text_seg->p_offset + text_seg->p_memsz);	/* changing old entry of the code by the address of ours */
 
 	ft_putstr("[+] Injecting fake jmp to last entry point...\n");
 	jmp_adr = (old_entry - (elf_begin->e_entry + code_size)); /* modifying JMP to get back to original entry */
 	ft_memcpy(jmp + 1, &jmp_adr, sizeof(int));
 
-	ft_printf("[+] Injecting code (%ld bytes) at offset %.8x (virtual adress 0x%.8x)...\n", code_size, CODE_OFFSET, CODE_ADRESS);
+	ft_printf("[+] Injecting code (%d bytes) at offset %.8x (virtual adress 0x%.8x)...\n", code_size, CODE_OFFSET, CODE_ADRESS);
 	insert_code(f_mmaped + CODE_OFFSET);
 
 	ft_putstr("[+] Updating segment size...\n");
-	elf_pt_load->p_memsz += code_size;
-	elf_pt_load->p_filesz += code_size;
+	text_seg->p_memsz += code_size;
+	text_seg->p_filesz += code_size;
 	return (0);
 }
